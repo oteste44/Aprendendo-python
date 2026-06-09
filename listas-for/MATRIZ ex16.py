072890dadc008230816c0a401285bff53007d6c0
oteste44 <oteste306@gmail.com>
2026-06-09 17:41:26 -0300

    aiiiii dos tudo

--- listas-for/MATRIZ ex16.py
+++ listas-for/MATRIZ ex16.py
@@ -1,12 +1,78 @@
-notas = [ ['joao', 2.3, 3.3],
-          ['mario', 5.7, 8.0],
-          ['beto', 9.0, 7.0]
+# notas = [ ['joao', 2.3, 3.3],
+#           ['mario', 5.7, 8.0],
+#           ['beto', 9.0, 7.0]
+#
+# ]
+# mediaAlunos = []
+# for i in notas:
+#     media = (i[1] + i[2])/2
+#     lista = (i[0],media)
+#     mediaAlunos.append(lista)
+# print(f'lista de notas do alunos: {notas}')
+# print(f'lista de media de alunos:{mediaAlunos}'
+#
+# estoque = [
+#
+#     [12, 5, 8],
+#
+#     [3, 15, 2],
+#
+#     [19, 0, 7]
+#
+# ]
+# prateleira = int(input('digite o numero da prateleira:'))
+# armario  = int(input('digite o numero do armario:'))
+#
+# for i in range(len(4))
+#     for c in i:
+# print([estoque][prateleira][armario])
 
-]
-mediaAlunos = []
-for i in notas:
-    media = (i[1] + i[2])/2
-    lista = (i[0],media)
-    mediaAlunos.append(lista)
-print(f'lista de notas do alunos: {notas}')
-print(f'lista de media de alunos:{mediaAlunos}')
+
+
+
+
+# vendas = [
+#     [1200, 850, 900, 1500],
+#     [900, 1100, 1000, 1300],
+#     [1500, 1600, 1400, 1800],
+#     [700, 600, 800, 900]
+# ]
+
+# vendas_vendedores = []
+#
+# for vendendor in vendas:
+#     soma = 0
+#     for dias in vendendor:
+#         soma += vendendor
+#         vendas_vendedores.append(soma)
+#
+#     vendas_dia = [0,0,0,0]
+#     for vendendor in range(len(vendas)):
+#         for dias in range(len(vendas[0])):
+#             vendas_dia[dias] += vendas[vendas_vendedores][dias]
+# ]
+# print(f'total de vendas por vendendor foi de R$:')
+# for i in range(len(vendas)):
+#     print(f'vendendor {i+1} = R$ {vendas_vendedores[i]:.2f}')
+#     print('')
+#     print('total de vendas por dia foi de R$:')
+#     for i in range(len(vendas)):
+#         print(f'Dia {i+1} = R$ {vendas_vendedores[i]:.2f}')
+#
+
+# matriz_a = [ [1,2,3],
+#              [4,5,6]
+# ]
+#
+#
+# matriz_t = [ [1,4],
+#              [2,5],
+#              [3,6]
+# ]
+#
+# matriz_t = [list(coluna) for coluna in zip(*matriz_a)]
+#
+#
+# for i in matriz_t:
+#     print(f'pisiçao{i+1} tem o valor')
+#
---
+++ listas-for/exercicio 10.py
@@ -0,0 +1,13 @@
+estoque = [
+
+    [12, 5, 8],
+
+    [3, 15, 2],
+
+    [19, 0, 7]
+
+]
+prateleira = int(input('digite o numero da prateleira:'))
+armario  = int(input('digite o numero do armario:'))
+
+print(estoque[prateleira][armario])
